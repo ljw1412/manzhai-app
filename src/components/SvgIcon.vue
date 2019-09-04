3afcd83628fbc61bc22f26c69f05e125caf65ead
ljw1412 <506644791@qq.com>
2019-09-04 23:47:02 +0800

    整合 svg icon

--- src/components/SvgIcon.vue
+++ src/components/SvgIcon.vue
@@ -1,13 +1,34 @@
 <template>
-  <svg class="svg-icon">
+  <svg class="svg-icon"
+    aria-hidden="true"
+    v-on="$listeners"
+    :style="svgStyles">
+    <use :xlink:href="iconName" />
   </svg>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator'
+import { Component, Vue, Prop } from 'vue-property-decorator'
 
 @Component
-export default class SVGIcon extends Vue {}
+export default class SVGIcon extends Vue {
+  @Prop({ required: true })
+  readonly name!: string
+
+  @Prop({ default: 20 })
+  readonly size!: number
+
+  get iconName() {
+    return `#icon-${this.name}`
+  }
+
+  get svgStyles() {
+    return {
+      width: this.size + 'px',
+      height: this.size + 'px'
+    }
+  }
+}
 </script>
 
 <style lang="scss" scoped>
--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import SvgIcon from '@/components/SvgIcon.vue'
+
+const svgs = require.context('@/icons', false, /\.svg$/)
+const requireAll = (requireContext: __WebpackModuleApi.RequireContext) =>
+  requireContext.keys().map(requireContext)
+requireAll(svgs)
 
 Vue.config.productionTip = false
+
+Vue.component('mz-icon', SvgIcon)
 
 new Vue({
   router,
